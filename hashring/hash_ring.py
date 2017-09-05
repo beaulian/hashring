83787a81be85a407dbf3abd657be8b140b16eb5d
shenaishiren <gjw.jesus@qq.com>
2017-09-05 23:12:38 +0800

    fix bug

--- hashring/hash_ring.py
+++ hashring/hash_ring.py
@@ -28,7 +28,7 @@ class HashRing(object):
     def get_proper_node(self, key):
         return self._node_policy.get_proper_node(key)
 
-    def add_node(self, node, weight):
+    def add_node(self, node, weight=DEFAULT_WEIGHT):
         average_weight = (self._total_weight + weight) / (len(self.nodes) + 1)
         vnode_count = self._gen_vnode_count(weight, average_weight)
         self._node_policy.add_node(node, vnode_count)
